c3dc08fd80f402e87bb71e28a7bf3c617cae51b6
Jefferson Asencios <jefferson30.ao@gmail.com>
2024-11-17 16:12:05 -0500

    NAVEGATION

--- app/src/main/java/com/example/voracesfisi/MainActivity.java
+++ app/src/main/java/com/example/voracesfisi/MainActivity.java
@@ -7,19 +7,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.voracesfisi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.cambiomonedas);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MoneyFragment());
+        binding.bottomNavigationView2.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.money) {
+                replaceFragment(new MoneyFragment());
+            } else if (itemId == R.id.person) {
+                replaceFragment(new ProfileFragment());
+            } else if (itemId == R.id.mochila) {
+                replaceFragment(new MochilaFragment());
+            } else {
+                return false;
+            }
+            return true;
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
+
 
